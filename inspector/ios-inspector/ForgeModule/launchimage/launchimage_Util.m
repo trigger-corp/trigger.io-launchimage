4875169893b0a50ba9b524124af3bd584484f055
Antoine van Gelder <antoine@trigger.io>
2014-09-18 12:35:22 +0200

    Setting hide-manually: true would fail to keep launchimage visible after app launch.

--- inspector/ios-inspector/ForgeModule/launchimage/launchimage_Util.m
+++ inspector/ios-inspector/ForgeModule/launchimage/launchimage_Util.m
@@ -17,25 +17,25 @@ UIImageView *launchImage;
 		if (floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_6_1) {
 			if (toInterfaceOrientation == UIInterfaceOrientationLandscapeLeft || toInterfaceOrientation == UIInterfaceOrientationLandscapeRight) {
 				if ([[UIScreen mainScreen] scale] >= 2) {
-					launchImage.image = [UIImage imageNamed:@"Default-700-Landscape@2x~ipad.png"];
+					launchImage.image = [UIImage imageNamed:@"LaunchImage-700-Landscape@2x~ipad.png"];
 				} else {
-					launchImage.image = [UIImage imageNamed:@"Default-700-Landscape~ipad.png"];
+					launchImage.image = [UIImage imageNamed:@"LaunchImage-700-Landscape~ipad.png"];
 				}
 				launchImage.frame = CGRectMake(0.0f, 0.0f, 1024.0f, 768.0f);
 			} else if (toInterfaceOrientation == UIInterfaceOrientationPortrait || toInterfaceOrientation == UIInterfaceOrientationPortraitUpsideDown) {
 				if ([[UIScreen mainScreen] scale] >= 2) {
-					launchImage.image = [UIImage imageNamed:@"Default-700-Portrait@2x~ipad.png"];
+					launchImage.image = [UIImage imageNamed:@"LaunchImage-700-Portrait@2x~ipad.png"];
 				} else {
-					launchImage.image = [UIImage imageNamed:@"Default-700-Portrait~ipad.png"];
+					launchImage.image = [UIImage imageNamed:@"LaunchImage-700-Portrait~ipad.png"];
 				}
 				launchImage.frame = CGRectMake(0.0f, 0.0f, 768.0f, 1024.0f);
 			}
 		} else {
 			if (toInterfaceOrientation == UIInterfaceOrientationLandscapeLeft || toInterfaceOrientation == UIInterfaceOrientationLandscapeRight) {
 				if ([[UIScreen mainScreen] scale] >= 2) {
-					launchImage.image = [UIImage imageNamed:@"Default-Landscape@2x~ipad.png"];
+					launchImage.image = [UIImage imageNamed:@"LaunchImage-Landscape@2x~ipad.png"];
 				} else {
-					launchImage.image = [UIImage imageNamed:@"Default-Landscape~ipad.png"];
+					launchImage.image = [UIImage imageNamed:@"LaunchImage-Landscape~ipad.png"];
 				}
 				if ([UIApplication sharedApplication].statusBarHidden) {
 					launchImage.frame = CGRectMake(0.0f, 0.0f, 1051.0f, 768.0f);
@@ -44,9 +44,9 @@ UIImageView *launchImage;
 				}
 			} else if (toInterfaceOrientation == UIInterfaceOrientationPortrait || toInterfaceOrientation == UIInterfaceOrientationPortraitUpsideDown) {
 				if ([[UIScreen mainScreen] scale] >= 2) {
-					launchImage.image = [UIImage imageNamed:@"Default-Portrait@2x~ipad.png"];
+					launchImage.image = [UIImage imageNamed:@"LaunchImage-Portrait@2x~ipad.png"];
 				} else {
-					launchImage.image = [UIImage imageNamed:@"Default-Portrait~ipad.png"];
+					launchImage.image = [UIImage imageNamed:@"LaunchImage-Portrait~ipad.png"];
 				}
 				if ([UIApplication sharedApplication].statusBarHidden) {
 					launchImage.frame = CGRectMake(0.0f, 0.0f, 783.0f, 1024.0f);
@@ -62,19 +62,19 @@ UIImageView *launchImage;
 		float y = 0.0f;
 		if ([[UIScreen mainScreen] scale] >= 2) {
 			if ([[UIScreen mainScreen] bounds].size.height == 568) {
-				launchImage.image = [UIImage imageNamed:@"Default-568h@2x~iphone.png"];
+				launchImage.image = [UIImage imageNamed:@"LaunchImage-568h@2x~iphone.png"];
 				height = 568.0f;
 			} else if ([[UIScreen mainScreen] bounds].size.height == 667) {
-				launchImage.image = [UIImage imageNamed:@"Default-667h@2x~iphone.png"];
+				launchImage.image = [UIImage imageNamed:@"LaunchImage-667h@2x~iphone.png"];
 				height = 667.0f;
 			} else if ([[UIScreen mainScreen] bounds].size.height == 736) {
-				launchImage.image = [UIImage imageNamed:@"Default-736h@3x~iphone.png"];
+				launchImage.image = [UIImage imageNamed:@"LaunchImage-736h@3x~iphone.png"];
 				height = 736.0f;
 			} else {
-				launchImage.image = [UIImage imageNamed:@"Default@2x~iphone.png"];
+				launchImage.image = [UIImage imageNamed:@"LaunchImage@2x~iphone.png"];
 			}
 		} else {
-			launchImage.image = [UIImage imageNamed:@"Default~iphone.png"];
+			launchImage.image = [UIImage imageNamed:@"LaunchImage~iphone.png"];
 		}
 		if (![UIApplication sharedApplication].statusBarHidden && floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_6_1) {
 			y = -20.0f;
